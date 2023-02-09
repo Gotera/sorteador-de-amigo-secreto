ed0ac07932008c839533101dd972b7e8d052cf7c
OM <orlandomedeiros.junior@gmail.com>
2023-02-09 16:40:39 -0300

    3.4

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,15 +1,56 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { RecoilRoot } from "recoil"
 import Footer from "./Footer"
+import { useParticipantsList } from "./state/hook/useParticipantsList"
 
-describe('onde não existem praticipantes suficientes', () => {
+jest.mock('./state/hook/useParticipantsList', () => {
+	return {
+		useParticipantsList: jest.fn()
+	}
+})
+
+const mockNavigate = jest.fn()
+
+jest.mock('react-router-dom', () => {
+	return {
+		useNavigate: () => mockNavigate
+	}
+})
+describe('quando não existem praticipantes suficientes', () => {
+	beforeEach(() => {
+		(useParticipantsList as jest.Mock).mockReturnValue([])
+	})
 	test('a brincadeira não pode ser iniciada', () => {
 		render(
 			<RecoilRoot>
 				<Footer />
 			</RecoilRoot>)
 
-			const botao = screen.getByRole('button')
-			expect(botao).toBeDisabled()
+		const botao = screen.getByRole('button')
+		expect(botao).toBeDisabled()
+	})
+})
+
+describe('quando existem praticipantes suficientes', () => {
+	beforeEach(() => {
+		(useParticipantsList as jest.Mock).mockReturnValue(['Ana', 'Rubens', 'Mario'])
+	})
+	test('a brincadeira pode ser iniciada', () => {
+		render(
+			<RecoilRoot>
+				<Footer />
+			</RecoilRoot>)
+		const botao = screen.getByRole('button')
+		expect(botao).not.toBeDisabled()
+	})
+	test('a brincadeira foi iniciada', () => {
+		render(
+			<RecoilRoot>
+				<Footer />
+			</RecoilRoot>)
+		const botao = screen.getByRole('button')
+		fireEvent.click(botao)
+		expect(mockNavigate).toHaveBeenCalledTimes(1)
+		expect(mockNavigate).toHaveBeenCalledWith('/sorteio')
 	})
 })
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom"
 import { useParticipantsList } from "./state/hook/useParticipantsList"
 
 const Footer = () => {
-	const participants = useParticipantsList
-	return(
+	const participants = useParticipantsList()
+	const navigate = useNavigate()
+
+	const start = () => {
+		navigate('/sorteio')
+	}
+	
+	return (
 		<footer>
-			<button disabled={participants.length < 3}>Iniciar Brincadeira</button>
+			<button disabled={participants.length < 3} onClick={start}>Iniciar Brincadeira</button>
 		</footer>
 	)
 }
